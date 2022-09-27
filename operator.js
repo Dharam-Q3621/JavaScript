2f32e630b60a90a2868ba57b997a5f6639955a40
Dharam-Q3621 <114222053+Dharam-Q3621@users.noreply.github.com>
2022-09-27 18:01:49 +0530

    javaScript  practices variable, operator

---
+++ operator.js
@@ -0,0 +1,39 @@
+
+let x = 5;
+let y = 3;
+
+
+console.log('x + y = ', x + y);  // 8
+
+console.log('x - y = ', x - y);  // 2
+
+console.log('x * y = ', x * y);  // 15
+
+console.log('x / y = ', x / y);  // 1.6666666666666667
+
+console.log('x % y = ', x % y);   // 2
+
+console.log('++x = ', ++x); // x is now 6
+console.log('x++ = ', x++); // prints 6 and then increased to 7
+console.log('x = ', x);     // 7
+
+
+console.log('--x = ', --x); // x is now 6
+console.log('x-- = ', x--); // prints 6 and then decreased to 5
+console.log('x = ', x);     // 5
+
+
+console.log('x ** y =', x ** y);
+
+const a = 3, b = 2;
+console.log(a > b); // true 
+
+// logical AND
+console.log(true && true); // true
+console.log(true && false); // false
+
+// logical OR
+console.log(true || false); // true
+
+// logical NOT
+console.log(!true); // false
